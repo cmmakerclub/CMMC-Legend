38ce897cd32ae227354ef79fa3e15e03b2778927
Nat Weerawan <nat@cmmc.io>
2018-04-07 18:45:59 +0700

    add webserver.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include <Arduino.h>
+#include <ESPAsyncTCP.h>
+#include <FS.h>
+#include <ESPAsyncWebServer.h>
+#include <SPIFFSEditor.h>
+
+
+const char* http_username = "admin";
+const char* http_password = "admin";
+
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+AsyncEventSource events("/events");
+
+#include "webserver.h"
+
+const char* hostName = "CMMC-Legend";
 
 void setup() {
-    // put your setup code here, to run once:
+    Serial.begin(57600);
+    Serial.setDebugOutput(true);
+    WiFi.hostname(hostName);
+    WiFi.mode(WIFI_AP_STA);
+    WiFi.softAP(hostName);
+    WiFi.begin("Nat", "espertap");    
+    SPIFFS.begin();
+    setupWebServer();
 }
 
 void loop() {
