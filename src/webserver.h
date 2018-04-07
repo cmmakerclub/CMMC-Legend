10b574243013754f0acd54274d12af16f89c50f1
Nat Weerawan <nat@cmmc.io>
2018-04-07 21:39:47 +0700

    add /api/mqtt with POST request.

--- src/webserver.h
+++ src/webserver.h
@@ -105,10 +105,24 @@ void setupWebServer() {
     request->send(200, "text/plain", String(ESP.getFreeHeap()));
   });
 
+server.on("/api/mqtt", HTTP_POST, [](AsyncWebServerRequest * request) {
+    int params = request->params();
+    for(int i=0;i<params;i++){
+      AsyncWebParameter* p = request->getParam(i);
+      if(p->isPost()){
+        const char* key = p->name().c_str();
+        const char* value = p->value().c_str();
+        Serial.printf("POST[%s]: %s\n", key, value);
+        configManager.add_field(key, value);        
+      }
+    }
+    configManager.commit();
+    request->send(200, "application/json", "{\"status\": \"OK!\"}");
+});
+
   server.on("/api/wifi/scan", HTTP_GET, [](AsyncWebServerRequest * request) {
     blinker->blink(100);
-	configManager.add_field("nat", "gosh!!");
-	configManager.commit();
+
     Serial.print(".....scan wifi >> ");
     // WiFi.disconnect();
     // delay(100);
