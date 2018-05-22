109e747371eee1614454e2b0083e830228c1584a
Nat <nat@cmmc.io>
2018-05-22 13:54:04 +0700

    Update library version for Vhello

--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
 /* Generated file, do not edit. */
-#define APP_VERSION "1.0.1"
-#define APP_VERSION_DATE "2018-05-22 13:49:25 +0700"
+#define APP_VERSION "hello"
+#define APP_VERSION_DATE "2018-05-22 13:54:04 +0700"
