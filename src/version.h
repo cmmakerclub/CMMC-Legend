950a760e6678c5010a23198d2e9d41093b25e7af
Nat <nat@cmmc.io>
2018-05-22 13:58:16 +0700

    update v1.0.1+build.8.gd9b6af4

--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
 /* Generated file, do not edit. */
-#define APP_VERSION "EMVER"
-#define APP_VERSION_DATE "2018-05-22 13:56:52 +0700"
+#define APP_VERSION "v1.0.1+build.8.gd9b6af4"
+#define APP_VERSION_DATE "2018-05-22 13:58:16 +0700"
