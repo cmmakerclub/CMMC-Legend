c7312a7c655650411ab60a85d459ad2b51f8c0a4
Nat <nat@cmmc.io>
2018-05-22 16:31:14 +0700

    Update library version for V1.0.1

--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
 /* Generated file, do not edit. */
-#define LEGEND_APP_VERSION "v1.0.0+build.9.g0b2cd99"
-#define LEGEND_APP_VERSION_DATE "2018-05-22 14:53:24 +0700"
+#define LEGEND_APP_VERSION "1.0.1"
+#define LEGEND_APP_VERSION_DATE "2018-05-22 16:31:14 +0700"
