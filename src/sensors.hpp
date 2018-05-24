a327a4b3c8acc17d1474553747d93d6f17265895
Nat Weerawan <nat@cmmc.io>
2018-05-24 13:55:10 +0700

    Update sensors.hpp

--- src/sensors.hpp
+++ src/sensors.hpp
@@ -30,36 +30,36 @@ class CMMC_DHT: public CMMC_Sensor {
         that->data.humidity = that->dht->readHumidity() * 100;
         that->cb((void*) &that->data, sizeof(that->data));
       });
-    } 
+    }
 };
 
 class CMMC_BME680: public CMMC_Sensor {
- private:
-   Adafruit_BME680 *bme; // I2C
- public:
-   typedef struct SENSOR_DATA {
-     uint32_t temperature;
-     uint32_t humidity;
-     uint32_t gas_resistance;
-     uint32_t pressure;
-   };
+  private:
+    Adafruit_BME680 *bme; // I2C
+  public:
+    typedef struct SENSOR_DATA {
+      uint32_t temperature;
+      uint32_t humidity;
+      uint32_t gas_resistance;
+      uint32_t pressure;
+    };
 
-   SENSOR_DATA data;
+    SENSOR_DATA data;
 
-   void setup() {
-     bme = new Adafruit_BME680;
-     if (!bme->begin()) {
-       Serial.println("Could not find a valid BME680 sensor, check wiring!");
-     }
-     else {
-       // Set up oversampling and filter initialization
-       bme->setTemperatureOversampling(BME680_OS_8X);
-       bme->setHumidityOversampling(BME680_OS_2X);
-       bme->setPressureOversampling(BME680_OS_4X);
-       bme->setIIRFilterSize(BME680_FILTER_SIZE_3);
-       bme->setGasHeater(320, 150); // 320*C for 150 ms
-     }
-   };
+    void setup() {
+      bme = new Adafruit_BME680;
+      if (!bme->begin()) {
+        Serial.println("Could not find a valid BME680 sensor, check wiring!");
+      }
+      else {
+        // Set up oversampling and filter initialization
+        bme->setTemperatureOversampling(BME680_OS_8X);
+        bme->setHumidityOversampling(BME680_OS_2X);
+        bme->setPressureOversampling(BME680_OS_4X);
+        bme->setIIRFilterSize(BME680_FILTER_SIZE_3);
+        bme->setGasHeater(320, 150); // 320*C for 150 ms
+      }
+    };
 
     void read() {
       static CMMC_BME680* that = this;
@@ -67,12 +67,12 @@ class CMMC_BME680: public CMMC_Sensor {
         if (!that->bme->performReading()) {
           Serial.println("Failed to perform reading :(");
           return;
-        } 
+        }
         that->data.temperature = that->bme->temperature * 100;
         that->data.humidity = that->bme->humidity * 100;
         that->data.pressure = that->bme->pressure;
         that->data.gas_resistance = that->bme->gas_resistance;
         that->cb((void*) &that->data, sizeof(that->data));
       });
-   };
+    };
 };
