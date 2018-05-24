9b4244b5f38686d01157457fe977a5d7b6086365
Nat <nat@cmmc.io>
2018-05-24 16:22:30 +0700

    update v1.0.1+build.36.gc2756a1

--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
 /* Generated file, do not edit. */
-#define LEGEND_APP_VERSION "v1.0.1+build.5.g8934a6f"
-#define LEGEND_APP_VERSION_DATE "2018-05-22 17:40:18 +0700"
+#define LEGEND_APP_VERSION "v1.0.1+build.36.gc2756a1"
+#define LEGEND_APP_VERSION_DATE "2018-05-24 16:20:18 +0700"
