1038821812df85dc566d3d14caec62f1cb716da3
Nat <nat@cmmc.io>
2018-05-26 16:11:40 +0700

    update v1.2.0+build.2.gb3e6d75

--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
 /* Generated file, do not edit. */
-#define LEGEND_APP_VERSION "1.2.0"
-#define LEGEND_APP_VERSION_DATE "2018-05-26 15:54:32 +0700"
+#define LEGEND_APP_VERSION "v1.2.0+build.2.gb3e6d75"
+#define LEGEND_APP_VERSION_DATE "2018-05-26 16:11:34 +0700"
