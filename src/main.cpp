03c10586037fff1fe289dbc8f8d599835b59fec9
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2018-05-29 18:34:54 +0700

    add HX711

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
 #include <Arduino.h> 
-#include <CMMC_18B20.hpp> 
-#include <CMMC_DHT.hpp> 
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 
@@ -9,6 +7,9 @@
 
 #include <CMMC_BME680.hpp>
 #include <CMMC_BME280.hpp>
+#include <CMMC_18B20.hpp> 
+#include <CMMC_DHT.hpp> 
+#include <CMMC_HX711.hpp>
 #include "gpio.hpp"
 
 #include "version.h"
@@ -54,28 +55,33 @@ void setup()
 {
   init_gpio();
   init_userconfig();
+
   Serial.println("USER MAIN SPACE.");
   Serial.printf("sensor Type = %s \r\n", sensorType);
-  String _sensorType = String(sensorType);
-  if (_sensorType == "BME280") {
-      sensorInstance = new CMMC_BME280;
-      sensorInstance->setup();
-  }
-  else if (_sensorType == "BME680") {
-      sensorInstance = new CMMC_BME680;
-      sensorInstance->setup();
-  }
-  else if (_sensorType == "DHT11") {
-      sensorInstance = new CMMC_DHT; 
-      sensorInstance->setup(dhtPin, 11);
-  }
-  else if (_sensorType == "DHT22") {
-      sensorInstance = new CMMC_DHT; 
-      sensorInstance->setup(dhtPin, 22);
-  }
-  else {
-    Serial.println("No sensor selected.");
-  }
+
+  // String _sensorType = String(sensorType);
+  // if (_sensorType == "BME280") {
+  //     sensorInstance = new CMMC_BME280;
+  //     sensorInstance->setup();
+  // }
+  // else if (_sensorType == "BME680") {
+  //     sensorInstance = new CMMC_BME680;
+  //     sensorInstance->setup();
+  // }
+  // else if (_sensorType == "DHT11") {
+  //     sensorInstance = new CMMC_DHT; 
+  //     sensorInstance->setup(dhtPin, 11);
+  // }
+  // else if (_sensorType == "DHT22") {
+  //     sensorInstance = new CMMC_DHT; 
+  //     sensorInstance->setup(dhtPin, 22);
+  // }
+  // else {
+  //   Serial.println("No sensor selected.");
+  // }
+
+  sensorInstance = new CMMC_HX711; 
+  sensorInstance->setup(12, 14);
 
   if (sensorInstance) {
     sensorInstance->every(10L * 1000);
@@ -86,7 +92,7 @@ void setup()
   select_bootmode();
   // Serial.setDebugOutput(true);
   // WiFi.begin("CMMC-3rd", "espertap");
-  Serial.printf("APP VERSION: %s\r\n", LEGEND_APP_VERSION);
+  // Serial.printf("APP VERSION: %s\r\n", LEGEND_APP_VERSION);
 }
 
 void loop()
