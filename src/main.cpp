6310ee0cd72a2a1c9b38850bc49fd985e9fa8d76
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2018-06-04 14:44:51 +0700

    add new LED.h and LED.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,12 @@ CMMC_Legend os;
 
 void setup()
 {
-<<<<<<< HEAD
   // os.addModule(new WiFiModule());
   // os.addModule(new ESPNowModule());
-=======
+
   os.addModule(new WiFiModule());
   os.addModule(new MqttModule);;
->>>>>>> b02ee2d17edf8d07a50bc2d4b5d27bffad56fec6
+
   os.setup();
   Serial.printf("APP VERSION: %s\r\n", LEGEND_APP_VERSION);
 }
