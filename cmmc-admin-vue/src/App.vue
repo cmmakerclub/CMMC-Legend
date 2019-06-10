e455902a5e0e4e1b9e62ebd5ade0a64d174cd3db
Gr4vity4 <prawared.bw@gmail.com>
2019-06-10 16:52:04 +0700

    Add ap-config design

--- cmmc-admin-vue/src/App.vue
+++ cmmc-admin-vue/src/App.vue
@@ -1,18 +1,23 @@
 <template>
     <div id="app">
-        <div class="container mx-auto">
+        <div class="container max-w-4xl mx-auto">
             <Navbar/>
+            <div class="min-h-screen border border-gray-300 pt-8 pl-8 pr-8">
+                <WiFiConfig/>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
   import Navbar from './components/Navbar'
+  import WiFiConfig from './components/WiFiConfig'
 
   export default {
     name: 'app',
     components: {
       Navbar,
+      WiFiConfig,
     },
   }
 </script>
