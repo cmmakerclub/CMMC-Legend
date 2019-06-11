819bc59378523e6aa3fa69c7475231857b8c5604
Gr4vity4 <prawared.bw@gmail.com>
2019-06-11 13:55:28 +0700

    Use tab toggle

--- cmmc-admin-vue/src/components/wifi/WiFiConfig.vue
+++ cmmc-admin-vue/src/components/wifi/WiFiConfig.vue
@@ -3,22 +3,22 @@
         <ul class="flex border-b">
             <li class="-mb-px mr-1">
                 <span class="bg-white inline-block py-2 px-4 text-blue-700 font-semibold"
-                      :class="{'border-l border-t border-r rounded-t' : isActiveAP}"
+                      :class="{'border-l border-t border-r rounded-t' : isTabToggle}"
                       style="cursor: pointer"
-                      @click="open_ap_tab">AP</span>
+                      @click="tabToggle">AP</span>
             </li>
             <li class="mr-1">
                 <span class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"
-                      :class="{'border-l border-t border-r rounded-t' : isActiveSTA}"
-                      @click="open_sta_tab">STA</span>
+                      :class="{'border-l border-t border-r rounded-t' : !isTabToggle}"
+                      @click="tabToggle">STA</span>
             </li>
         </ul>
 
-        <div v-if="isActiveAP">
+        <div v-if="isTabToggle">
             <AP/>
         </div>
 
-        <div v-if="isActiveSTA">
+        <div v-if="!isTabToggle">
             <STA/>
         </div>
 
@@ -37,18 +37,12 @@
     },
     data: function () {
       return {
-        isActiveAP: true,
-        isActiveSTA: false,
+        isTabToggle: true,
       }
     },
     methods: {
-      open_ap_tab: function () {
-        this.isActiveAP = true
-        this.isActiveSTA = false
-      },
-      open_sta_tab: function () {
-        this.isActiveAP = false
-        this.isActiveSTA = true
+      tabToggle: function () {
+        this.isTabToggle = !this.isTabToggle
       },
     },
     mounted () {
